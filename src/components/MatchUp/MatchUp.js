3133342fabe0a12ee72c2f2621b24da83f3c86f7
Jessica Justice <jessbjust@gmail.com>
2021-04-21 20:33:32 -0600

    Build out skeleton MatchUp component

---
+++ src/components/MatchUp/MatchUp.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import './MatchUp.css';
+
+
+class MatchUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    }
+  }
+
+  render() {
+    return (
+      <h1>hello</h1>
+    )
+  }
+
+
+
+}
+
+export default MatchUp;
