32cbf1d803109794663d589f8f941ec7190a16cb
Jessica Justice <jessbjust@gmail.com>
2021-04-23 20:45:50 -0600

    Render stats and apply MVP styling

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./Stats.css";
+import image from '../MatchUp/android17-main1.png';
 
 const Stats = ({ fighter }) => {
-  console.log(fighter)
+
+  const allMoves = fighter.moves.map(move => {
+      return(
+        <li>{ move }</li>
+      )
+    })
+
   return (
     <main className="stats-container">
-      <h1> Stats</h1>
-      <p>{fighter.name}</p>
-
-
-
-
+      <h1 className="title">Stats</h1>
+      <img
+        className="image"
+        src={ image }
+      />
+      <div className="first-column">
+        <p>{ fighter.summary }</p>
+        <p>{ fighter.rank }</p>
+        <p>{ fighter.fighting_style }</p>
+      </div>
+      <div className="second-column">ABILITIES: { allMoves }</div>
     </main>
   )
 }
