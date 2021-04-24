e78d7ebf7749e030262faa44542b408530653ad2
Jessica Justice <jessbjust@gmail.com>
2021-04-23 21:06:10 -0600

    Add message if no character is selected for stats page

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,27 +4,34 @@ import image from '../MatchUp/android17-main1.png';
 
 const Stats = ({ fighter }) => {
 
-  const allMoves = fighter.moves.map(move => {
-      return(
-        <li>{ move }</li>
-      )
-    })
+  if (fighter.name) {
 
-  return (
-    <main className="stats-container">
-      <h1 className="title">Stats</h1>
-      <img
-        className="image"
-        src={ image }
-      />
-      <div className="first-column">
-        <p>{ fighter.summary }</p>
-        <p>{ fighter.rank }</p>
-        <p>{ fighter.fighting_style }</p>
-      </div>
-      <div className="second-column">ABILITIES: { allMoves }</div>
-    </main>
-  )
+    const allMoves = fighter.moves.map(move => {
+        return(
+          <li>{ move }</li>
+        )
+      })
+
+    return (
+      <main className="stats-container">
+        <h1 className="title">Stats</h1>
+        <img
+          className="image"
+          src={ image }
+        />
+        <div className="first-column">
+          <p>{ fighter.summary }</p>
+          <p>{ fighter.rank }</p>
+          <p>{ fighter.fighting_style }</p>
+        </div>
+        <div className="second-column">ABILITIES: { allMoves }</div>
+      </main>
+    )
+  } else {
+    return (
+      <p>Make a selection to see some stats!</p>
+    )
+  }
 }
 
 export default Stats;
